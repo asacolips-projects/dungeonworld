42d06c0347806a9c6c09c52187f9fd84389f796f
Matt Smith <matt.smith@mediacurrent.com>
2020-12-31 11:28:00 -0600

    Refactor artifact build

--- update-build-tags.js
+++ update-build-tags.js
@@ -15,7 +15,7 @@ const argv = yargs
       description: 'The git tag used for this version (CI_COMMIT_TAG)'
     })
     .option('versionpre', {
-      type: 'number',
+      type: 'string',
       description: 'specifies the timestamp as a prefix on beta builds (CI_COMMIT_TIMESTAMP)'
     })
     .demandOption(['branch', 'gitlabpath'])
@@ -25,16 +25,24 @@ const systemRaw = fs.readFileSync('./dist/system.json');
 let system = JSON.parse(systemRaw);
 
 // Calculate the version.
+console.log(argv);
 if (argv.branch == 'beta' && argv.versionpre) {
-  system.version = `beta${argv.versionpre}-${system.version}`;
+  if (system.nextVersion) {
+    system.version = `beta${argv.versionpre}-${system.nextVersion}`;
+  }
+  else {
+    system.version = `beta${argv.versionpre}-${system.version.replace(/beta\d*-/g, '')}`;
+  }
 }
 else if (argv.version) {
   system.version = argv.version;
 }
+
+delete system.nextVersion;
 
 system.url = `https://gitlab.com/${argv.gitlabpath}`;
 system.manifest = `https://gitlab.com/${argv.gitlabpath}/-/jobs/artifacts/${argv.branch}/raw/system.json?job=build`;
 system.download = `https://gitlab.com/${argv.gitlabpath}/-/jobs/artifacts/${argv.branch}/raw/dungeonworld.zip?job=build`;
 
 fs.writeFileSync('./dist/system.json', JSON.stringify(system, null, 2));
-console.log(system.manifest);
+console.log({version: system.version, manifest: system.manifest});
