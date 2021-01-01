73f49aac7a88560ceb43fac8425cd43e02730d75
Matt Smith <matt.smith@mediacurrent.com>
2020-12-31 19:33:28 -0600

    Release 0.6.7

--- update-build-tags.js
+++ update-build-tags.js
@@ -47,7 +47,7 @@ let artifactVersion = argv.tag ? argv.tag : argv.branch;
 
 // Update URLs.
 system.url = `https://gitlab.com/${argv.gitlabpath}`;
-system.manifest = `https://gitlab.com/${argv.gitlabpath}/-/jobs/artifacts/-/raw/${artifactBranch}/system.json`;
+system.manifest = `https://gitlab.com/${argv.gitlabpath}/-/raw/${artifactBranch}/system.json`;
 system.download = `https://gitlab.com/${argv.gitlabpath}/-/jobs/artifacts/${artifactVersion}/raw/dungeonworld.zip?job=${argv.jobname}`;
 
 fs.writeFileSync('./dist/system.json', JSON.stringify(system, null, 2));
