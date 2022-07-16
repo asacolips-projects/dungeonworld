7eb154a607ec75eb2c6f305c20bc0825ef9f6915
Matt Smith <mattsmith142003@gmail.com>
2022-07-15 20:10:20 -0500

    Merge branch 'master' into 92-v10-compatibility-no-bc

--- src/module/dungeonworld.js
+++ src/module/dungeonworld.js
@@ -601,12 +601,16 @@ Hooks.on('renderDialog', (dialog, html, options) => {
  * @returns {Promise}
  */
 async function createDwMacro(data, slot) {
+  // First, determine if this is a valid owned item.
   if (data.type !== "Item") return;
-  // @todo v10: improve this.
-  if (!("system" in data)) return ui.notifications.warn("You can only create macro buttons for owned Items");
-  const item = data;
+  if (!data.uuid.includes('Actor.') && !data.uuid.includes('Token.')) {
+    return ui.notifications.warn("You can only create macro buttons for owned Items");
+  }
+  // If it is, retrieve it based on the uuid.
+  const item = await Item.fromDropData(data);
 
   // Create the macro command
+  // @todo refactor this to use uuids and folders.
   const command = `game.dungeonworld.rollItemMacro("${item.name}");`;
   let macro = game.macros.find(m => (m.name === item.name) && (m.command === command));
   if (!macro) {
@@ -615,7 +619,10 @@ async function createDwMacro(data, slot) {
       type: "script",
       img: item.img,
       command: command,
-      flags: { "dungeonworld.itemMacro": true }
+      flags: {
+        "dungeonworld.itemMacro": true,
+        "dungeonworld.itemUuid": data.uuid
+      }
     });
   }
   game.user.assignHotbarMacro(macro, slot);
@@ -625,17 +632,39 @@ async function createDwMacro(data, slot) {
 /**
  * Create a Macro from an Item drop.
  * Get an existing item macro if one exists, otherwise create a new one.
- * @param {string} itemName
+ * @param {string} itemData
  * @return {Promise}
  */
-function rollItemMacro(itemName) {
-  const speaker = ChatMessage.getSpeaker();
-  let actor;
-  if (speaker.token) actor = game.actors.tokens[speaker.token];
-  if (!actor) actor = game.actors.get(speaker.actor);
-  const item = actor ? actor.items.find(i => i.name === itemName) : null;
-  if (!item) return ui.notifications.warn(`Your controlled Actor does not have an item named ${itemName}`);
+function rollItemMacro(itemData) {
+  // Reconstruct the drop data so that we can load the item.
+  // @todo this section isn't currently used, the name section below is used.
+  if (itemData.includes('Actor.') || itemData.includes('Token.')) {
+    const dropData = {
+      type: 'Item',
+      uuid: itemData
+    };
+    Item.fromDropData(dropData).then(item => {
+      // Determine if the item loaded and if it's an owned item.
+      if (!item || !item.parent) {
+        const itemName = item?.name ?? itemData;
+        return ui.notifications.warn(`Could not find item ${itemName}. You may need to delete and recreate this macro.`);
+      }
 
-  // Trigger the item roll
-  return item.roll();
+      // Trigger the item roll
+      item.roll();
+    });
+  }
+  // Load item by name from the actor.
+  else {
+    const speaker = ChatMessage.getSpeaker();
+    const itemName = itemData;
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    const item = actor ? actor.items.find(i => i.name === itemName) : null;
+    if (!item) return ui.notifications.warn(`Your controlled Actor does not have an item named ${itemName}`);
+
+    // Trigger the item roll
+    return item.roll();
+  }
 }
