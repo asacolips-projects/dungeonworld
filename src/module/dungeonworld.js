da6be30094c54fc335def82c872292382ce77558
Guilhem Doulcier <guilhem.doulcier@ens.fr>
2023-04-16 11:54:41 +0200

    Fix the drag-and-drop behaviour of moves from the character sheet.

--- src/module/dungeonworld.js
+++ src/module/dungeonworld.js
@@ -306,7 +306,7 @@ Hooks.once("init", async function() {
 
 Hooks.once("ready", async function() {
   // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
-  Hooks.on("hotbarDrop", (bar, data, slot) => createDwMacro(data, slot));
+  Hooks.on("hotbarDrop", (bar, data, slot) => {createDwMacro(data, slot); return false});
 
   DW.classlist = await DwClassList.getClasses();
   CONFIG.DW = DW;
